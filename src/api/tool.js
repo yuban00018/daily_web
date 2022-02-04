e8f0c4057ad9c68a4508c6440f573fc942f8cc0f
yuban00018 <caozhuowen2013@outlook.com>
2022-02-04 23:25:36 +0800

    [UPDATE] statistic page

--- src/api/tool.js
+++ src/api/tool.js
@@ -1,5 +1,24 @@
 export function Reload(){
-    var {search,href} = window.location;
+    let {search,href} = window.location;
     href = href.replace(/&?t_reload=(\d+)/g,'')
     window.location.href = href+(search?'&':'?')+"t_reload="+new Date().getTime()
+}
+Date.prototype.format = function(format)
+{
+    const o = {
+        "M+": this.getMonth() + 1, //month
+        "d+": this.getDate(),    //day
+        "h+": this.getHours(),   //hour
+        "m+": this.getMinutes(), //minute
+        "s+": this.getSeconds(), //second
+        "q+": Math.floor((this.getMonth() + 3) / 3),  //quarter
+        "S": this.getMilliseconds() //millisecond
+    };
+    if(/(y+)/.test(format)) format=format.replace(RegExp.$1,
+        (this.getFullYear()+"").substr(4 - RegExp.$1.length));
+    for(let k in o)if(new RegExp("("+ k +")").test(format))
+        format = format.replace(RegExp.$1,
+            RegExp.$1.length==1 ? o[k] :
+                ("00"+ o[k]).substr((""+ o[k]).length));
+    return format;
 }
--- src/views/statistic/statistic.vue
+++ src/views/statistic/statistic.vue
@@ -1,14 +1,72 @@
 <template>
-<div>
-  <v-avatar color="teal">
-    <span class="white--text text-h5">曹</span>
-  </v-avatar>
-</div>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="12">
+        <v-card>
+          <v-img
+              height="60px"
+              src="https://cdn.pixabay.com/photo/2020/07/12/07/47/bee-5396362_1280.jpg"
+          >
+            <v-card-title style="color: azure" v-text="title"></v-card-title>
+          </v-img>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <v-sheet height="500">
+          <v-calendar
+              :now="today"
+              :value="today"
+              color="primary"
+          >
+            <template v-slot:day="{ past, date }">
+              <v-row
+                  class="fill-height"
+              >
+                <template v-if="past && tracked[date]">
+                  <v-sheet
+                      v-for="(percent, i) in tracked[date]"
+                      :key="i"
+                      :title="category[i]"
+                      :color="colors[i]"
+                      :width="`${percent}%`"
+                      height="100%"
+                      tile
+                  ></v-sheet>
+                </template>
+              </v-row>
+            </template>
+          </v-calendar>
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import '@/api/tool'
+
 export default {
-  name: "statistic"
+  name: "statistic",
+  created() {
+    let today = new Date();
+    let dayDiff = (new Date() - new Date(localStorage.getItem("signUpDate"))) / 1000 / 60 / 60 / 24;
+    let max = 30 < Math.round(dayDiff) ? 30 : Math.round(dayDiff);
+    for (let i = 1; i <= max; i++) {
+      let day = new Date(today - 1000 * 60 * 60 * 24 * i);
+      this.tracked[day.format("yyyy-MM-dd")] = [55, 45];
+    }
+  },
+  data() {
+    return {
+      title: '近30天完成度',
+      today: '',
+      tracked: {},
+      colors: ['primary','warning'],
+      category: ['完成', '未完成'],
+    }
+  }
 }
 </script>
 
