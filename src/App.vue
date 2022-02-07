5b5ab908534976362cd3261106f098010fbfa345
yuban00018 <caozhuowen2013@outlook.com>
2022-02-07 12:34:09 +0800

    [ADD] logout button test, nonfunctional

--- src/App.vue
+++ src/App.vue
@@ -42,9 +42,39 @@
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>Daily</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
+      <v-menu
+          transition="slide-y-transition"
+          offset-y
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+              icon
+              v-bind="attrs"
+              v-on="on"
+          >
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item-group
+              v-model="selectedVertical"
+              active-class="deep-purple--text text--accent-4"
+          >
+            <v-list-item
+              v-for="(item,i) in verticalList"
+              :key="i"
+          >
+              <v-list-item-icon>
+                <v-icon v-text="item.icon"></v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title v-text="item.text"></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-menu>
     </v-app-bar>
 
     <!-- 根据应用组件来调整你的内容 -->
@@ -71,6 +101,7 @@ export default {
 
   data: () => ({
     drawer: false,
+    selectedVertical:null,
     selectedMenu: null,
     menu: [
       { text: '今日待办', icon: 'mdi-format-list-checks' },
@@ -78,6 +109,7 @@ export default {
       { text: '统计数据', icon: 'mdi-chart-line' },
       //{ text: '小组', icon: 'mdi-account-multiple'}
     ],
+    verticalList: [{text:'登出',icon: 'mdi-logout'}]
   }),
 
   watch: {
@@ -86,6 +118,9 @@ export default {
       let path = ['/todo','/plan','statistic','Group']
       this.$router.push(path[this.selectedMenu])
     },
+    selectedVertical(){
+      console.log(this.selectedVertical)
+    }
   },
 };
 </script>
